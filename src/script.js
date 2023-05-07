b4a97adaa5c0d12ed976be661593310ea9f655bc
Ramen-Noodle <clee970@yahoo.com>
2023-05-07 01:38:13 -0400

    Dropdown menu close on click

--- src/script.js
+++ src/script.js
@@ -312,6 +312,15 @@ function resetChars() {
 }
 
 function changeSong(songIndex) {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    console.log("1");
+  } else {
+    x.style.display = "block";
+    console.log("2");
+  }
+  
   currentIdx = songIndex;
   currentUrl = SongList[songIndex].url;
   currentBeatId = SongList[songIndex].beatId;
