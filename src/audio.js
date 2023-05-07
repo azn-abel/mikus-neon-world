2e5f22ccfa608e681c867b6b1965d06b5546e75b
Abel Lu <aluprof4@ncsu.edu>
2023-05-06 22:14:38 -0400

    fixed audio

--- src/audio.js
+++ src/audio.js
@@ -2,6 +2,8 @@ var audio = new Audio('To_live.mp3');
 var audioCtx;
 var analyser;
 var audioElement;
+var dataArray;
+var sum = 0;
 
 const wrapper = () => {
     initializeAudio(audioElement);
@@ -18,9 +20,9 @@ const initializeAudio = () => {
     }
 }
  
-const handleAudioElement = (audioElement) => {
+const handleAudioElement = (audioElement) => { 
     console.log(audioElement); 
-    audioElement.src = './audio/' + currentIdx + '.mp3';
+    audioElement.src = 'audio/' + currentIdx + '.mp3';
     // Connect the audio element to the analyser node
     var source = audioCtx.createMediaElementSource(audioElement);
     console.log(audioCtx.sampleRate);
@@ -36,11 +38,13 @@ const handleAudioElement = (audioElement) => {
     var x = 0;
     function updateFrequencyData() {
         if (x % 100 == 0) {
-            console.log(JSON.stringify(dataArray));                
+            // console.log(JSON.stringify(dataArray));
+            console.log(sum);              
         }
         x++;
 
         analyser.getByteFrequencyData(dataArray);
+        sum = calculateSum(dataArray);
 
         // Process the frequency data
         // Here, you can access the frequency data in the dataArray
@@ -56,27 +60,43 @@ const handleAudioElement = (audioElement) => {
 var observer = new MutationObserver(function(mutationsList) {
     // Check each mutation for added nodes
     mutationsList.forEach(function(mutation) {
-      var addedNodes = mutation.addedNodes;
-  
-      // Check if any of the added nodes are <audio> elements
-      for (var i = 0; i < addedNodes.length; i++) {
-        if (addedNodes[i].nodeName === 'AUDIO') {
-            audioElement = addedNodes[i];
-            try {
-                initializeAudio();
-            } catch (error) {
-                document.addEventListener('click', initializeAudio);
+        var addedNodes = mutation.addedNodes;
+    
+        // Check if any of the added nodes are <audio> elements
+        for (var i = 0; i < addedNodes.length; i++) {
+            if (addedNodes[i].nodeName === 'AUDIO') {
+                audioElement = addedNodes[i];
+
+                document.addEventListener("click", initializeAudio());
+
+                // Disconnect the observer since we no longer need it
+                // observer.disconnect();
+                return;
             }
-            // Disconnect the observer since we no longer need it
-            // observer.disconnect();
-            return;
         }
-      }
     });
-  });
+});
   
   // Start observing changes to the document
-  observer.observe(document, { childList: true, subtree: true });
+function observe() {
+    observer.observe(document, { childList: true, subtree: true });
+    try {
+        document.removeEventListener('click', observe);
+    } catch {
+
+    }
+}
+// observer.observe(document, { childList: true, subtree: true });
+
+document.addEventListener('click', observe);
+
+function calculateSum(arr) {
+    var s = 0;
+    for (var i = 0; i < arr.length; i++) {
+        s += arr[i];
+    }
+    return s;
+}
   
 
 
